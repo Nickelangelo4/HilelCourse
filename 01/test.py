74380e31e1ccdd5e3cd6a5160c4c0f99ac2fb2e5
nikita <ilusenkoit@gmail.com>
2024-10-29 21:02:46 +0200

    first commit

--- 01/test.py
+++ 01/test.py
@@ -1,1 +1,13 @@
 print('Hello world')
+
+number = int(input("Введіть 4-значне число: "))
+
+user_number1 = number // 1_000
+user_number2 = (number % 1_000) // 1_00
+user_number3 = (number %1_00) // 10
+user_number4 = number % 10
+
+print(user_number1)
+print(user_number2)
+print(user_number3)
+print(user_number4)
