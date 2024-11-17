61b2297d55f0cd11c8b1f2a5eb5f974d1a687dc2
nikita <ilusenkoit@gmail.com>
2024-11-17 15:36:32 +0200

    first commit

--- 01/03.py
+++ 01/03.py
@@ -1,11 +1,5 @@
-input_list1 = [1, 2, 3, 4, 5, 6]
-input_list2 = [1, 2, 3]
-input_list3 = [1, 2, 3, 4, 5]
-input_list4 = [1]
-input_list5 = []
-
-input_list = input_list1
-middle_index = (len(input_list) + 1) // 2 if input_list else 0
-result = [input_list[:middle_index], input_list[middle_index:]]
-
+lst = [0, 12, 0, 5, 3]
+non_zero_elements = [x for x in lst if x != 0]
+zero_count = lst.count(0)
+result = non_zero_elements + [0] * zero_count
 print(result)
